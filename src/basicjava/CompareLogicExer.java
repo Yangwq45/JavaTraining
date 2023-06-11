955ac7373ea376db7ac5bcc6f8c9ec1a2d1fac50
YwQxz <iyangwq@outlook.com>
2023-06-11 09:17:56 +0800

    first

---
+++ src/basicjava/CompareLogicExer.java
@@ -0,0 +1,16 @@
+package basicjava;
+/*
+@author YwQ
+@version 1.0
+*/
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class CompareLogicExer {
+    public static void main(String[] args) {
+
+
+
+    }
+}
---
+++ src/太656.java
@@ -0,0 +1,16 @@
+/*
+@author YwQ
+@version 1.0
+*/
+
+public class 太656 {
+
+    public static void main(String[] args) {
+        int hour=89;
+        int left=89%24;
+        System.out.println("5+5="+5+5+12+"1212");
+    int a,b,c;
+
+
+    }
+}
