a6d8baed2927f46b52e2f0961fd68a47967ef7e6
YwQxz <iyangwq@outlook.com>
2023-06-14 10:40:07 +0800

    Changes getter and setter

--- src/LearnOop/GetandSet/Books.java
+++ src/LearnOop/GetandSet/Books.java
@@ -5,4 +5,39 @@ package LearnOop.GetandSet;
 */
 
 public class Books {
+    double price;
+    String name;
+    String auther,Publisher;
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuther() {
+        return auther;
+    }
+
+    public void setAuther(String auther) {
+        this.auther = auther;
+    }
+
+    public String getPublisher() {
+        return Publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        Publisher = publisher;
+    }
 }
